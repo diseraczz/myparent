e84ee9489740859c73299320c3beb3ce2452082c
diseraczz <59109490+diseraczz@users.noreply.github.com>
2020-05-27 22:51:27 +0700

    แก้ให้อัพรูปได้

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-const image = document.getElementById('salad')
+const image = document.getElementById('outputs')
 const canvas = document.getElementById('canvas')
 const outp = document.getElementById('output')
 const context = canvas.getContext('2d')
 context.drawImage(image,0,0)
-async function run() {
+const loadFile = function(event) {
+    const output = document.getElementById('outputs');
+        output.src = URL.createObjectURL(event.target.files[0]);
+        output.onload = function() {
+            URL.revokeObjectURL(outputs.src) // free memory
+        }
+};
+
+function dett(){
+    const image = document.getElementById('outputs')
+    const canvas = document.getElementById('canvas')
+    const outp = document.getElementById('output')
+    const context = canvas.getContext('2d')
+    context.drawImage(image,0,0)
+    run();
     
+}
+
+async function run() {
     const model = await tf.automl.loadObjectDetection('model.json');
-    const image = document.getElementById('salad');
+    const image = document.getElementById('outputs');
     const options = {score: 0.5, iou: 0.5, topk: 20};
     const predictions = await model.detect(image, options);
     console.log(predictions);
     // Show the resulting object on the page.
+    
+    ///////////////////////////////////
     for(var i = 0; i<predictions.length; i++){
         bbox = [
             predictions[i].box.left,
@@ -18,7 +37,7 @@ async function run() {
             predictions[i].box.width,
             predictions[i].box.height
         ]
-        outp.innerHTML += "<br/>" + predictions[i].label + " : Score = " + predictions[i].score + " : box = " + bbox;
+        image.innerHTML += "<br/>" + predictions[i].label + " : Score = " + predictions[i].score + " : box = " + bbox;
         context.beginPath();
         context.rect(bbox[0], bbox[1], bbox[2], bbox[3]);
         context.lineWidth = 3;
@@ -28,12 +47,8 @@ async function run() {
         context.fillText(predictions[i].label, bbox[0]+4, bbox[1]+12);
     }
     /////
-    const pre = document.createElement('pre');
-    pre.textContent = JSON.stringify(predictions, null, 2);
-    document.body.append(pre);
-    
-
+    //const pre = document.createElement('pre');
+    //pre.textContent = JSON.stringify(predictions, null, 2);
+    //document.body.append(pre);
 }
-run();
-
     
